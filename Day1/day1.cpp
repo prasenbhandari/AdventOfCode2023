7cb842da2f602a527e355b40574eadcc242ebe36
prasenbhandari <107975283+prasenbhandari@users.noreply.github.com>
2023-12-02 10:30:32 +0530

    Update day1.cpp

--- Day1/day1.cpp
+++ Day1/day1.cpp
@@ -24,12 +24,9 @@ int main(){
     std::vector<int> num;
     std::vector<std::string> lines;
 
-
     std::map<std::string, std::string> num_word{
         {"one", "o1e"}, {"two", "t2o"}, {"three", "t3e"}, {"four", "f4r"}, {"five", "f5e"}, {"six", "s6x"}, {"seven", "s7n"}, {"eight", "e8t"}, {"nine", "n9e"}, {"zero", "z0o"}
     };
-
-
 
     //part 1
     while(std::getline(input_file,line)){
@@ -46,17 +43,6 @@ int main(){
             }
         }
 
-        //for(const auto& j: num_word){   //replace word with number
-        //    int pos = v.find(j.first);
-        //    if(pos != std::string::npos){
-        //        v.replace(pos, j.first.length(), j.second);
-        //    }
-        //}
-
-        //cout << "[" << v << "]\n";
-
-        char front = '\0', back;
-
         for(auto i: v){ //check for number
             if(std::isdigit(i)){
                 if(front == '\0')
@@ -65,11 +51,10 @@ int main(){
                 back = i;
             }
         }
-        //cout << "[" << front << back << "]\n";
         num.emplace_back(concatenate(front, back));
     }
 
-    int sum = std::accumulate(num.begin(), num.end(), 0);
+    int sum = std::accumulate(num.begin(), num.end(), 0); //add the values
     cout << sum;
 
 }
